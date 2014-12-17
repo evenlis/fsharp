cadf351f065be618f3edf64c63bf8e4edb3ac675
Even Lislebø <e.lislebo@gmail.com>
2014-12-17 19:00:59 +0100

    Pattern matching on list

--- pattern-matching.fs
+++ pattern-matching.fs
@@ -1,3 +1,6 @@
+let length = List.length
+let join = List.concat
+
 let fun1 a =
   match a with
     | "foo" -> "bar"
@@ -10,5 +13,27 @@ let fun2 =
     | "answer" -> "42"
     | _ -> "No match"
 
+let list_matching list =
+  match list with
+    | [] -> "empty list"
+    | [a] -> "list with one element"
+    | [a;_] -> "list with two elements"
+    | _ -> "list with several elements"
+
+let head_tail_matching list =
+  match list with
+    | [] -> "empty list"
+    | [_] -> "list with one element"
+    | _::tail -> sprintf "list of length %d" <| (length tail + 1)
+
 printfn "%s" <| fun1 "asdasdlkjlkj"
 printfn "%s" <| fun2 ("foo")
+
+printfn "%s" <| list_matching [1;2;3]
+printfn "%s" <| list_matching [1;2]
+printfn "%s" <| list_matching [1]
+printfn "%s" <| list_matching []
+
+printfn "%s" <| head_tail_matching [1;2;3]
+printfn "%s" <| head_tail_matching [1]
+printfn "%s" <| head_tail_matching []
